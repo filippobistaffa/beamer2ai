3fca169ad5787ae920e2ad86f09fc66e54571d6d
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2024-12-14 00:30:13 +0100

    Changed variables' names

--- make-video.py
+++ make-video.py
@@ -22,7 +22,7 @@ import subprocess
 import tempfile
 
 
-def text_to_audio(text, audio_path, voice_preset):
+def text_to_audio(text, output_audio_path, voice_preset):
     print("Generating audio from sentences...")
     silence = np.zeros(int(0.25 * SAMPLE_RATE))
     sentences = text.split(".")
@@ -33,13 +33,13 @@ def text_to_audio(text, audio_path, voice_preset):
         print(sentence.strip())
         audio_array = generate_audio(sentence.strip() + '.', history_prompt=voice_preset)
         audio_chunks += [audio_array, silence.copy()]
-    write_wav(audio_path, rate=SAMPLE_RATE, data=np.concatenate(audio_chunks))
+    write_wav(output_audio_path, rate=SAMPLE_RATE, data=np.concatenate(audio_chunks))
 
 
-def page_audio_to_video(pdf_path, page_number, audio_path, video_path, resolution):
+def page_audio_to_video(input_pdf_path, page_number, input_audio_path, output_video_path, resolution):
     with tempfile.NamedTemporaryFile(suffix=".png") as temp_image:
         print("Encoding video and muxing audio...")
-        doc = fitz.open(pdf_path)
+        doc = fitz.open(input_pdf_path)
         page = doc[page_number - 1]
         pix = page.get_pixmap(dpi=600)
         pix.save(temp_image.name)
@@ -48,7 +48,7 @@ def page_audio_to_video(pdf_path, page_number, audio_path, video_path, resolutio
             "-y",
             "-loop", "1",
             "-i", temp_image.name,
-            "-i", audio_path,
+            "-i", input_audio_path,
             "-c:v", "libx264",
             "-tune", "stillimage",
             "-c:a", "aac",
@@ -56,30 +56,30 @@ def page_audio_to_video(pdf_path, page_number, audio_path, video_path, resolutio
             "-vf", "scale={}:{}".format(*resolution),
             "-pix_fmt", "yuv420p",
             "-shortest",
-            video_path
+            output_video_path
         ]
         with open(os.devnull, "w") as devnull:
             subprocess.run(ffmpeg_command, check=True, stdout=devnull, stderr=devnull)
         doc.close()
 
 
-def text_page_to_video(text, voice_preset, pdf_path, page_number, video_path, resolution):
+def text_page_to_video(text, voice_preset, input_pdf_path, page_number, output_video_path, resolution):
     with tempfile.NamedTemporaryFile(suffix=".wav") as temp_audio:
         text_to_audio(
             text=text,
-            audio_path=temp_audio.name,
+            output_audio_path=temp_audio.name,
             voice_preset=voice_preset
         )
         page_audio_to_video(
-            pdf_path=pdf_path,
+            input_pdf_path=input_pdf_path,
             page_number=page_number,
-            audio_path=temp_audio.name,
-            video_path=video_path,
+            input_audio_path=temp_audio.name,
+            output_video_path=output_video_path,
             resolution=resolution
         )
 
 
-def concatenate_chunks(temp_chunks, video_path):
+def concatenate_chunks(temp_chunks, output_video_path):
     print("Concatenating video chunks...")
     with tempfile.NamedTemporaryFile(suffix=".txt") as temp_list:
         with open(temp_list.name, "w") as f:
@@ -92,28 +92,28 @@ def concatenate_chunks(temp_chunks, video_path):
             "-safe", "0",
             "-i", temp_list.name,
             "-c", "copy",
-            video_path
+            output_video_path
         ]
         with open(os.devnull, "w") as devnull:
             subprocess.run(ffmpeg_command, check=True, stdout=devnull, stderr=devnull)
     
 
-def generate_video(scripts, voice_preset, pdf_path, video_path, resolution):
+def generate_video(input_pdf_path, scripts, voice_preset, output_video_path, resolution):
     temp_chunks = []
     for script in scripts:
         temp_chunk = tempfile.NamedTemporaryFile(suffix=".mp4")
         temp_chunks.append(temp_chunk)
         text_page_to_video(
             text=script["text"],
-            page_number=script["page_number"],
+            page_number=script["pdf_page_number"],
             voice_preset=voice_preset,
-            pdf_path=pdf_path,
-            video_path=temp_chunk.name,
+            input_pdf_path=input_pdf_path,
+            output_video_path=temp_chunk.name,
             resolution=resolution
         )
     concatenate_chunks(
-        temp_chunks,
-        video_path
+        temp_chunks=temp_chunks,
+        output_video_path=output_video_path
     )
     for temp_chunk in temp_chunks:
         temp_chunk.close()
@@ -125,18 +125,18 @@ if __name__ == "__main__":
     voice_preset = "v2/en_speaker_8"
 
     generate_video(
+        input_pdf_path="../main.pdf",
         scripts=[
             {
-                "text": 'Hello everyone, welcome to the presentation of our work entitled "Recommending Green Routes for Pedestrians to Reduce the Exposure to Air Pollutants in Barcelona". This is joint work between the Artificial Intelligence Research Institute and the UPF University in Barcelona.',
-                "page_number": 1,
+                "text": 'Welcome to the presentation of our work entitled "Recommending Green Routes for Pedestrians to Reduce the Exposure to Air Pollutants in Barcelona". This is joint work between the Artificial Intelligence Research Institute and the UPF University in Barcelona.',
+                "pdf_page_number": 1,
             },
             {
                 "text": 'The main issue we tackle in this work is how to recommend green routes to pedestrians that minimize the exposure to air pollutants in a city. Pedestrians are the most vulnerable individuals when it comes to air pollution, which can lead to serious health problems.',
-                "page_number": 2,
+                "pdf_page_number": 2,
             },
         ],
         voice_preset=voice_preset,
-        pdf_path="../main.pdf",
-        video_path="output.mp4",
+        output_video_path="output.mp4",
         resolution=(3840, 2160)
     )
